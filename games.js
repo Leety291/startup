d5b3a223783016815a9345a1d6f6432f8ce480b7
Leety291 <tylee080712@gmail.com>
2025-10-23 01:11:27 +0900

    Add files via upload

--- games.js
+++ games.js
@@ -31,8 +31,8 @@ const games = [
     },
     {
         id: 4,
-        title: "똥피하기",
-        description: "여기에 새로운 게임에 대한 설명을 입력해주세요.",
+        title: "ThrowDodge",
+        description: "ThrowDodge는 2인용 턴제 웹 게임으로, 한 플레이어는 위에서 투사체를 던져 공격하고 다른 플레이어는 아래에서  피하며 방어합니다. 네온 스타일의 그래픽과 다양한 아이템, 역할 교대 시스템을 통해 전략적인 심리전을 즐길 수 있습니다.",
         thumbnail: "",
         link: "https://leety291.github.io/ThrowDodge/"
     }
